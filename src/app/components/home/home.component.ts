41755f01c136ff2e822b322a033247367dee9d9d
Zavaglia <simone.zavaglia93@gmail.com>
2024-12-17 12:02:00 +0100

    Sistema Avatar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -50,8 +50,25 @@ resumeLang2: " ", resumeSkill1: " ", resumeSkill2: " ", resumeSkill3: " ",
 resumeSkill4: " ", resumeSkill5: " ", resumeSkill6: " ", resumeSkill7: " ",
 jobTitle1: " ", jobPlace1: " "};
 
-
-
+selectedPhrase:string="";
+key: keyof typeof this.avatarLanguage = "aboutLocked";
+preAvatarLang:any;
+avatarLanguage={   
+  aboutLocked:"Ciao sono <b>Simone</b>, questo è il mio sito portfolio.<br> Sarò la tua guida qui dentro.<br> Puoi navigare su questa versione o su quella più seria, se preferisci.",
+  aboutMoving:"E' mio dovere seguirti in questa navigazione. Altrimenti che guida sarei?",
+  heroReturn:"Uff! Stavo soffocando là sotto!",
+  project1:"La parte più complicata è stata la gestione di ogni singolo livello. Se vuoi, puoi provare il gioco su itch.io. Funziona tutto, fino alla fine… Credo.",
+  project2:"Tutti i programmatori, prima o poi, hanno avuto a che fare coi Pokémon. Ammettiamolo dai.",
+  project3:"Sapete cosa dovremmo fare? APRIRE UN BAR! Questa era una cit. ad How i Met Your- lascia perdere… Non dovevo distrarti.",
+  project4:"Fare un sito per un parente è come correre con le forbici in mano e i lacci slacciati.",
+  resume1: "In ambienti diversi ho imparato una miriade di cose diverse. So lavorare a contatto con la gente e dietro le quinte, da solo e in gruppo e ho ottime doti comunicative.",
+  resume2: "I lavori che ho fatto non c'entrano nulla con la programmazione, lo so. Ma cosa non si fa per campare eh?",
+  contacts:"Spero che la mia personalità e le mie capacità ti abbiano almeno incuriosito. Se vuoi sapere di più o vuoi contattarmi, beh… Sai già cosa fare!",
+  hidle1:"Sei ancora li? Ti sei alzato a prendere un caffè?<br>Sbrighiamoci però, tra un paio d’ore devo fare l’avatar digitale su un’app di trading.",
+  hidle2:"E' imbarazzante stare qui fermo a non fare nulla...",
+  return1:"Ma si guardiamo un'altra volta questa parte!",
+  return2:"Non l'avevamo già visto questo?",
+  return3:"Questa sezione non è niente male lo so."}
 
 
   constructor(private animationService: AvatarAnimationsService,
@@ -66,6 +83,7 @@ jobTitle1: " ", jobPlace1: " "};
       }else{
       this.selectedLang=lang;
       this.languageService.changeLanguage(lang);
+      this.selectedPhrase=this.avatarLanguage[this.key];
       }
     }
 
@@ -89,11 +107,17 @@ jobTitle1: " ", jobPlace1: " "};
 
     // Aggiungi la classe quando si supera la sezione di 50px
     if (sectionTop <= -50 && !this.isTriggered) {
+      console.log(this.avatarLanguage)
+      this.key="aboutMoving"
+      this.selectedPhrase=this.avatarLanguage[this.key];
       this.fixAvatar = true;
       this.isTriggered = true;
       this.animationService.startAnimation();
       this.removeScrollListener(); // Disabilita il listener dopo il primo trigger
     } if (projectSectionTop <=300 && !this.projTriggered ) {
+      console.log(this.avatarLanguage)
+      this.key="project1"
+      this.selectedPhrase=this.avatarLanguage[this.key];
       this.selectedProj=0;
       this.animationStates[0] = 'visible';
       this.projTriggered=true;
@@ -163,11 +187,17 @@ getBaloonSource(){
   ngOnInit(): void {
     if(this.isPlatFormBrowser===true){
     this.addScrollListener();
+    console.log(this.avatarLanguage)
     window.setTimeout(() => {
     this.onScroll()
     this.languageService.selectedLanguage$.subscribe((lang) => {
       this.prelang = lang; // Lingua non sanificata
       this.language = this.languageService.sanitizeLanguage(lang);
+    });
+    this.languageService.selectedLanguageAvatar$.subscribe((avatarLang) => {
+      this.preAvatarLang = avatarLang 
+      this.avatarLanguage = this.languageService.sanitizeLanguage(avatarLang) // Avatar sanitizzato
+      this.selectedPhrase = this.avatarLanguage[this.key];
     });
     },0);
   }
--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -23,7 +23,8 @@ export class LanguageService {
     proj4Title:"Sito commerciale - Ace of Cups", proj4Utilized:"<b>Fatto con:</b>Angular, Css", proj4Description:"In questo progetto ho creato una vetrina digitale per un cocktail bar dal carattere mistico, con un’esperienza altamente interattiva realizzata grazie ad animazioni fluide con <b>Angular Animations</b> e dati mockati per simulare un ambiente reale.<br> Il menù consente di visualizzare e gestire gli ordini in modo <b>chiaro</b> e <b>intuitivo</b>, collegandoli al proprio tavolo tramite un<br> <b>codice univoco</b> e monitorando in tempo reale la spesa complessiva.<br> La sezione shop integra funzionalità avanzate ispirate all’<b>e-commerce</b>, permettendo di scegliere,<br> modificare e acquistare merchandise ufficiale con un sistema di carrello versatile, che consente di<br> sfogliare, selezionare e modificare il contenuto prima dell'acquisto, il tutto per offrire un’esperienza utente innovativa e coinvolgente.",
     resumeEdu:"Titolo di Studio:",resumeEdu2:"Diploma Scientifico",resumeLang:"Lingue",resumeLang2:"Italiano - Inglese <span>(C1)</span>", resumeSkill1:"Apprendimento Rapido",resumeSkill2:"Multipotenziale", resumeSkill3:"Lavoro di Squadra",
     resumeSkill4:"Team Building", resumeSkill5:"Creatività",resumeSkill6:"Performante sotto stress",resumeSkill7:"Amichevole e Disponibile",
-    jobTitle1:"Commesso",jobPlace1:"Cartolibreria l'Apostrofo",
+    jobTitle1:"Commesso",jobPlace1:"Cartolibreria l'Apostrofo",jobTitle2:"S.Media Manager",jobPlace2:"",jobTitle3:"Venditore",jobPlace3:"Intimo U-sel",
+    jobTitle14:"Commesso",jobPlace4:"Cartolibreria l'Apostrofo",
   };
 
   english={   
@@ -34,27 +35,69 @@ export class LanguageService {
     aboutDescription:'Namei&#39;s Simone and i&#39;m <s> 30</s> <span>31</span> years old. I&#39;m a noisy and lively guy that loves a lot of things and hates at least twice as much. About me, people says that i talk a lot, that i&#39;m <s>Do-No-Wrong Guy </s> <span id="span2">meticulous</span> and that i spend all my time<br> on the PC. But i have some flaws too! <br>Stare fermo a non fare nulla mi mette ansia, per questo cerco di dare sempre un senso alle mie giornate in maniera produttiva: <br>Scrivo, programmo, leggo, ascolto o guardo qualcosa che mi <s>fomenti</s> <span id="span3">ispiri </span>.<br> Mi piace imparare cose sempre nuove e migliorare ciò che già conosco, tanto che non riesco a star dietro alle N-mila passioni che ho.<br>Ho la fortuna di avere belle persone accanto che mi ricordano sempre che senza gli altri, senza "gioco di squadra", non si arriva mai lontano.',
     aboutListLove:"<b>Loves</b>",aboutListLove1:"Games",aboutListLove2:"Cards",aboutListLove3:"Coffee",
     aboutListHate:"<b>Hates</b>",aboutListHate1:"Svegliarmi presto",aboutListHate2:"Il caldo",aboutListHate3:"Chi mi tocca <br>mentre parla",
-    proj1Utilized:"<b>Fatto con:</b>Angular, Css, Node.js", proj1Description:'Questo progetto rappresenta il mio lavoro <b>fullstack</b> più completo, integrando ogni aspetto dello sviluppo, dalla grafica al database, per creare un gioco completo con 13 livelli. Il database non relazionale, implementato con <b>Node.js</b> e <b>Mongoose</b>, gestisce dinamicamente musiche, immagini, livelli e dati di gioco. Grazie alla struttura <b>asincrona</b> del codice, i dati vengono salvati e aggiornati in tempo reale.Il sistema sfrutta <b>caricamenti lazy</b> per suoni e immagini, ottimizzando le performance tramite <b>caching</b> e riducendo i tempi di attesa. La modularità dell’intero gioco permette una gestione efficiente dei componenti, mentre le <b>route guards</b> limitano la navigazione in base ai dati dell’utente, offrendo un’esperienza coinvolgente simile a un videogame, ma realizzata come un sito web tramite Angular.',
-    proj2Utilized:"<b>Fatto con:</b>Javascript e CSS", proj2Description:'Una settimana dopo aver studiato Javascript ho iniziato questo <b>primo progetto</b> imparando a programmare parallelamente alla sua edificazione.In circa quindici giorni ho creato un giochino <b>one page</b> basato sulli&#39;uso di <b>finestre modali</b> che emula lo stile di un browser game anni 2000/10.I dati sono <b>mockati</b> alli&#39;interno dello script in array di oggetti che tengono conto dei pokemon, delle loro statistiche e delle loro possibili mosse. La gestione delle musiche, dei suoni e delle animazioni è molto basilare ma fa il suo lavoro, con funzioni più complesse create per la gestione delli&#39; <b>IA</b> delli&#39;avversario.<br>Il gioco comprende anche un pokedex funzionante grazie alli&#39;integrazione di un <b>API</b> esterna.<br> Questo progetto, seppur il mio primo, mi ha insegnato tantissimo ed è come il primo amore:<br> Non si scorda mai.',
-    proj3Title:"Sito Vetrina Personale - ZG Model & Actor", proj3Utilized:"<b>Fatto con:</b>Angular, Css, Swiper.js",proj3Description:"Ho sviluppato siti web personali per un attore e un modello, focalizzati su un design minimalista ed elegante. La hero section, realizzata con la libreria Swiper.js, include CTA chiare e dirette per catturare immediatamente l’attenzione. Ogni sezione del sito è animata con elementi semplici ma efficaci, che mantengono leggerezza e fluidità. La sezione dedicata ai ruoli svolti presenta un carosello dinamico in stile piattaforma di streaming, con dati aggiornati in tempo reale. Ho inoltre integrato profili social e contenuti video per garantire un rapido accesso alle principali piattaforme e campagne marketing del soggetto. L’interfaccia consente un contatto diretto e immediato attraverso i canali principali, facilitando richieste di informazioni o opportunità di ingaggio",
-    proj4Title:"Sito commerciale - Ace of Cups", proj4Utilized:"<b>Fatto con:</b>Angular, Css", proj4Description:"In questo progetto ho creato una vetrina digitale per un cocktail bar dal carattere mistico, con un’esperienza altamente interattiva realizzata grazie ad animazioni fluide con <b>Angular Animations</b> e dati mockati per simulare un ambiente reale.<br> Il menù consente di visualizzare e gestire gli ordini in modo <b>chiaro</b> e <b>intuitivo</b>, collegandoli al proprio tavolo tramite un<br> <b>codice univoco</b> e monitorando in tempo reale la spesa complessiva.<br> La sezione shop integra funzionalità avanzate ispirate all’<b>e-commerce</b>, permettendo di scegliere,<br> modificare e acquistare merchandise ufficiale con un sistema di carrello versatile, che consente di<br> sfogliare, selezionare e modificare il contenuto prima dell'acquisto, il tutto per offrire un’esperienza utente innovativa e coinvolgente.",
-    resumeEdu:"Titolo di Studio:",resumeEdu2:"Diploma Scientifico",resumeLang:"Lingue",resumeLang2:"Italiano - Inglese <span>(C1)</span>", resumeSkill1:"Apprendimento Rapido",resumeSkill2:"Multipotenziale", resumeSkill3:"Lavoro di Squadra",
+    proj1Utilized:"<b>Made with:</b>Angular, Css, Node.js", proj1Description:'Questo progetto rappresenta il mio lavoro <b>fullstack</b> più completo, integrando ogni aspetto dello sviluppo, dalla grafica al database, per creare un gioco completo con 13 livelli. Il database non relazionale, implementato con <b>Node.js</b> e <b>Mongoose</b>, gestisce dinamicamente musiche, immagini, livelli e dati di gioco. Grazie alla struttura <b>asincrona</b> del codice, i dati vengono salvati e aggiornati in tempo reale.Il sistema sfrutta <b>caricamenti lazy</b> per suoni e immagini, ottimizzando le performance tramite <b>caching</b> e riducendo i tempi di attesa. La modularità dell’intero gioco permette una gestione efficiente dei componenti, mentre le <b>route guards</b> limitano la navigazione in base ai dati dell’utente, offrendo un’esperienza coinvolgente simile a un videogame, ma realizzata come un sito web tramite Angular.',
+    proj2Utilized:"<b>Made with:</b>Javascript e CSS", proj2Description:'Una settimana dopo aver studiato Javascript ho iniziato questo <b>primo progetto</b> imparando a programmare parallelamente alla sua edificazione.In circa quindici giorni ho creato un giochino <b>one page</b> basato sulli&#39;uso di <b>finestre modali</b> che emula lo stile di un browser game anni 2000/10.I dati sono <b>mockati</b> alli&#39;interno dello script in array di oggetti che tengono conto dei pokemon, delle loro statistiche e delle loro possibili mosse. La gestione delle musiche, dei suoni e delle animazioni è molto basilare ma fa il suo lavoro, con funzioni più complesse create per la gestione delli&#39; <b>IA</b> delli&#39;avversario.<br>Il gioco comprende anche un pokedex funzionante grazie alli&#39;integrazione di un <b>API</b> esterna.<br> Questo progetto, seppur il mio primo, mi ha insegnato tantissimo ed è come il primo amore:<br> Non si scorda mai.',
+    proj3Title:"Personal showcase site - ZG Model & Actor", proj3Utilized:"<b>Fatto con:</b>Angular, Css, Swiper.js",proj3Description:"Ho sviluppato siti web personali per un attore e un modello, focalizzati su un design minimalista ed elegante. La hero section, realizzata con la libreria Swiper.js, include CTA chiare e dirette per catturare immediatamente l’attenzione. Ogni sezione del sito è animata con elementi semplici ma efficaci, che mantengono leggerezza e fluidità. La sezione dedicata ai ruoli svolti presenta un carosello dinamico in stile piattaforma di streaming, con dati aggiornati in tempo reale. Ho inoltre integrato profili social e contenuti video per garantire un rapido accesso alle principali piattaforme e campagne marketing del soggetto. L’interfaccia consente un contatto diretto e immediato attraverso i canali principali, facilitando richieste di informazioni o opportunità di ingaggio",
+    proj4Title:"Commercial site - Ace of Cups", proj4Utilized:"<b>Fatto con:</b>Angular, Css", proj4Description:"In questo progetto ho creato una vetrina digitale per un cocktail bar dal carattere mistico, con un’esperienza altamente interattiva realizzata grazie ad animazioni fluide con <b>Angular Animations</b> e dati mockati per simulare un ambiente reale.<br> Il menù consente di visualizzare e gestire gli ordini in modo <b>chiaro</b> e <b>intuitivo</b>, collegandoli al proprio tavolo tramite un<br> <b>codice univoco</b> e monitorando in tempo reale la spesa complessiva.<br> La sezione shop integra funzionalità avanzate ispirate all’<b>e-commerce</b>, permettendo di scegliere,<br> modificare e acquistare merchandise ufficiale con un sistema di carrello versatile, che consente di<br> sfogliare, selezionare e modificare il contenuto prima dell'acquisto, il tutto per offrire un’esperienza utente innovativa e coinvolgente.",
+    resumeEdu:"Education:",resumeEdu2:"Diploma Scientifico",resumeLang:"Languages",resumeLang2:"Italiano - Inglese <span>(C1)</span>", resumeSkill1:"Apprendimento Rapido",resumeSkill2:"Multipotenziale", resumeSkill3:"Lavoro di Squadra",
     resumeSkill4:"Team Building", resumeSkill5:"Creatività",resumeSkill6:"Performante sotto stress",resumeSkill7:"Amichevole e Disponibile",
     jobTitle1:"Commesso",jobPlace1:"Cartolibreria l'Apostrofo",
 
   };
+
+  balloonIta={
+    aboutLocked:"Ciao sono <b>Simone</b>, questo è il mio sito portfolio.<br> Sarò la tua guida qui dentro.<br> Puoi navigare su questa versione o su quella più seria, se preferisci.",
+    aboutMoving:"E' mio dovere seguirti in questa navigazione. Altrimenti che guida sarei?",
+    heroReturn:"Uff! Stavo soffocando là sotto!",
+    project1:"La parte più complicata è stata la gestione di ogni singolo livello. Se vuoi, puoi provare il gioco su itch.io. Funziona tutto, fino alla fine… Credo.",
+    project2:"Tutti i programmatori, prima o poi, hanno avuto a che fare coi Pokémon. Ammettiamolo dai.",
+    project3:"Sapete cosa dovremmo fare? APRIRE UN BAR! Questa era una cit. ad How i Met Your- lascia perdere… Non dovevo distrarti.",
+    project4:"Fare un sito per un parente è come correre con le forbici in mano e i lacci slacciati.",
+    resume1: "In ambienti diversi ho imparato una miriade di cose diverse. So lavorare a contatto con la gente e dietro le quinte, da solo e in gruppo e ho ottime doti comunicative.",
+    resume2: "I lavori che ho fatto non c'entrano nulla con la programmazione, lo so. Ma cosa non si fa per campare eh?",
+    contacts:"Spero che la mia personalità e le mie capacità ti abbiano almeno incuriosito. Se vuoi sapere di più o vuoi contattarmi, beh… Sai già cosa fare!",
+    hidle1:"Sei ancora li? Ti sei alzato a prendere un caffè?<br>Sbrighiamoci però, tra un paio d’ore devo fare l’avatar digitale su un’app di trading.",
+    hidle2:"E' imbarazzante stare qui fermo a non fare nulla...",
+    return1:"Ma si guardiamo un'altra volta questa parte!",
+    return2:"Non l'avevamo già visto questo?",
+    return3:"Questa sezione non è niente male lo so."
+  };
+
+  balloonEng={
+    aboutLocked:"Hi! I'm <b>Simone</b>, this is my portfolio site.<br> Sarò la tua guida qui dentro.<br> Puoi navigare su questa versione o su quella più seria, se preferisci.",
+    aboutMoving:"It's my duty to follow you during this navigation. Altrimenti che guida sarei?",
+    heroReturn:"Uff! Stavo soffocando là sotto!",
+    project1:"La parte più complicata è stata la gestione di ogni singolo livello. Se vuoi, puoi provare il gioco su itch.io. Funziona tutto, fino alla fine… Credo.",
+    project2:"Tutti i programmatori, prima o poi, hanno avuto a che fare coi Pokémon. Ammettiamolo dai.",
+    project3:"Sapete cosa dovremmo fare? APRIRE UN BAR! Questa era una cit. ad How i Met Your- lascia perdere… Non dovevo distrarti.",
+    project4:"Fare un sito per un parente è come correre con le forbici in mano e i lacci slacciati.",
+    resume1: "In ambienti diversi ho imparato una miriade di cose diverse. So lavorare a contatto con la gente e dietro le quinte, da solo e in gruppo e ho ottime doti comunicative.",
+    resume2: "I lavori che ho fatto non c'entrano nulla con la programmazione, lo so. Ma cosa non si fa per campare eh?",
+    contacts:"Spero che la mia personalità e le mie capacità ti abbiano almeno incuriosito. Se vuoi sapere di più o vuoi contattarmi, beh… Sai già cosa fare!",
+    hidle1:"Sei ancora li? Ti sei alzato a prendere un caffè?<br>Sbrighiamoci però, tra un paio d’ore devo fare l’avatar digitale su un’app di trading.",
+    hidle2:"E' imbarazzante stare qui fermo a non fare nulla...",
+    return1:"Ma si guardiamo un'altra volta questa parte!",
+    return2:"Non l'avevamo già visto questo?",
+    return3:"Questa sezione non è niente male lo so."
+  }
+
+
   private selectedLanguage = new BehaviorSubject<any>(this.italian); // Italiano come lingua predefinita
   selectedLanguage$ = this.selectedLanguage.asObservable();
+  private selectedLanguageAvatar = new BehaviorSubject<any>(this.balloonIta); // Italiano come lingua predefinita
+  selectedLanguageAvatar$ =this.selectedLanguageAvatar.asObservable();
 
   constructor(private sanitizer: DomSanitizer) {}
 
   // Metodo per cambiare lingua
   changeLanguage(selezione: string): void {
     if (selezione === 'ITA' && this.selectedLanguage.getValue() !== this.italian) {
-      this.selectedLanguage.next(this.italian);
+      this.selectedLanguage.next(this.sanitizeLanguage(this.italian));
+      this.selectedLanguageAvatar.next(this.sanitizeLanguage(this.balloonIta));
     }
     if (selezione === 'ENG' && this.selectedLanguage.getValue() !== this.english) {
-      this.selectedLanguage.next(this.english);
+      this.selectedLanguage.next(this.sanitizeLanguage(this.english));
+      this.selectedLanguageAvatar.next(this.sanitizeLanguage(this.balloonEng));
     }
   }
 
@@ -70,4 +113,5 @@ export class LanguageService {
     }
     return sanitized;
   }
+
 }
