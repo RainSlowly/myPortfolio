6db4bf5727ea834fa80186a324cf7ee5b2433206
RainSlowly <simone.zavaglia93@gmail.com>
2025-03-27 13:19:07 +0100

    Deploy aggiornato

--- dist/mio-portfolio/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-K5OETBK5.mjs";import"./chunk-DCCN2A5C.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-6X5YSEL4.mjs";import"./chunk-DCCN2A5C.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
